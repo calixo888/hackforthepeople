d96e76e644e8714cf7538338ef648588388865f4
Calix Huang <calix.huang1@gmail.com>
2020-06-29 20:56:31 -0700

    setup about section

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,15 @@ export default () => {
 
       <section className="about">
         <div className="container">
-          <h1>About</h1>
+          <div className="about-grid">
+            <div className="image" style={{ height: "300px" }}></div>
+            <div>
+              <h1>About</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
 
