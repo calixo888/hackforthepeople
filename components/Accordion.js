90759a255fb11fb6076b3d712874db0ea6589778
Calix Huang <calix.huang1@gmail.com>
2020-07-05 14:42:01 -0700

    added faq page + styling

---
+++ components/Accordion.js
@@ -0,0 +1,27 @@
+import styles from '../styles/modules/accordion.module.scss'
+
+export default ({ title, content }) => {
+  const extend = (e) => {
+    const panel = e.target.parentNode.nextSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
+
+  return (
+    <div className={styles.accordion}>
+      <div onClick={extend}>
+        <h4>
+          {title}
+        </h4>
+      </div>
+      <div className={styles.panel}>
+        <br/>
+        <p>{content}</p>
+        <br/>
+      </div>
+    </div>
+  )
+}
--- pages/faqs.js
+++ pages/faqs.js
@@ -1,9 +1,22 @@
 import Layout from '../components/Layout.js';
+import Header from '../components/Header.js';
+import Accordion from '../components/Accordion.js';
 
 export default () => {
   return (
     <Layout>
-      <h1>FAQs</h1>
+      <Header headerText="Frequently Asked Questions" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+
+      <section className="faqs">
+        <div className="container">
+          <Accordion title="Question" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
+          <Accordion title="Question" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
+          <Accordion title="Question" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
+          <Accordion title="Question" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
+          <Accordion title="Question" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
+          <Accordion title="Question" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
+        </div>
+      </section>
     </Layout>
   )
 }
