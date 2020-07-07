6014c85bdd5dbce9ad238f64c74a7123eaa29f1d
Calix Huang <calix.huang1@gmail.com>
2020-07-06 18:48:18 -0700

    setup table

--- pages/schedule.js
+++ pages/schedule.js
@@ -1,9 +1,41 @@
 import Layout from '../components/Layout.js';
+import Header from '../components/Header.js';
 
 export default () => {
   return (
     <Layout>
-      <h1>Schedule</h1>
+      <Header headerText="Virtual Schedule" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+
+      <section className="schedule">
+        <div className="container">
+          <table>
+            <tr>
+              <th>Test</th>
+              <th>Test</th>
+              <th>Test</th>
+              <th>Test</th>
+            </tr>
+            <tr>
+              <td>Test</td>
+              <td>Test</td>
+              <td>Test</td>
+              <td>Test</td>
+            </tr>
+            <tr>
+              <td>Test</td>
+              <td>Test</td>
+              <td>Test</td>
+              <td>Test</td>
+            </tr>
+            <tr>
+              <td>Test</td>
+              <td>Test</td>
+              <td>Test</td>
+              <td>Test</td>
+            </tr>
+          </table>
+        </div>
+      </section>
     </Layout>
   )
 }
