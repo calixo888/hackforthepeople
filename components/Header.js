5e64e33555a7c1b96a5dabdf187ccaffbb8470f4
Calix Huang <calix.huang1@gmail.com>
2020-07-13 12:58:58 -0700

    added banner

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,7 @@ import styles from '../styles/modules/header.module.scss'
 export default ({ headerText, description }) => {
   return (
     <header className={styles.header}>
+      <img src="/img/banners/about.svg" alt="" />
       <div>
         <h1>{headerText}</h1>
         <p>{description}</p>
