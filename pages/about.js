840d4748a7ea0eb280801722c00caf268ecb167a
Michael Wang <michaelwwang17@gmail.com>
2020-07-14 13:41:02 -0400

    add calix

--- pages/about.js
+++ pages/about.js
@@ -1,59 +1,61 @@
-import Layout from '../components/Layout.js';
-import Header from '../components/Header.js';
-import Head from 'next/head';
+import Layout from "../components/Layout.js";
+import Header from "../components/Header.js";
+import Head from "next/head";
 
 export default () => {
   return (
     <Layout>
       <Head>
         <title>Hack For The People - About</title>
-        <meta name="description" content="About Hack For The People, an inter-collegiate online hackathon powered by the Coronavirus Visualization Team!" />
+        <meta
+          name="description"
+          content="About Hack For The People, an inter-collegiate online hackathon powered by the Coronavirus Visualization Team!"
+        />
       </Head>
 
-      <Header headerText="About Hack For The People" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+      <Header
+        headerText="About Hack For The People"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      />
 
       <section className="goals">
         <div className="container">
           <div className="goals-header">
             <h1>Goals</h1>
             <p>
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat
             </p>
           </div>
           <div className="goals-grid mt-5">
             <div className="goal">
               <img src="/img/team/test.jpg" />
               <h3>Goal</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
             <div className="goal">
               <img src="/img/team/test.jpg" />
               <h3>Goal</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
             <div className="goal">
               <img src="/img/team/test.jpg" />
               <h3>Goal</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
             <div className="goal">
               <img src="/img/team/test.jpg" />
               <h3>Goal</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
         </div>
       </section>
 
-      <Header headerText="Meet the Team!" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+      <Header
+        headerText="Meet the Team!"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      />
       <section className="team">
         <div className="container">
           <div className="team-grid">
@@ -78,7 +80,7 @@ export default () => {
               <p>Director of Outreach</p>
             </div>
             <div className="member">
-              <img src="/img/team/default.jpg" />
+              <img src="/img/team/calix.jpeg" />
               <h5>Calix Huang</h5>
               <p>Director of Technology</p>
             </div>
@@ -174,5 +176,5 @@ export default () => {
         </div>
       </section>
     </Layout>
-  )
-}
+  );
+};
