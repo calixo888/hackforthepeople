6d5ef8dbd7b64b53c1ecdc97b9ac44b8c3497d7a
Michael Wang <michaelwwang17@gmail.com>
2020-07-15 00:31:38 -0400

    removed about header

--- pages/about.js
+++ pages/about.js
@@ -14,11 +14,6 @@ export default () => {
       </Head>
 
       <Header
-        headerText="About Hack For The People"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-      />
-
-      <Header
         headerText="Meet the Team!"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
       />
