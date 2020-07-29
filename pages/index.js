08cc397b8950fed5ee21fac4cc0da05634c02a3f
Michael Wang <michaelwwang17@gmail.com>
2020-07-28 22:16:00 -0400

    moved logos from part to spons

--- pages/index.js
+++ pages/index.js
@@ -436,38 +436,41 @@ export default () => {
                 <img src="/img/logos/partners/discord.svg" />
               </a>
             </div>
-            <div className="partner p-4">
-              <a href="https://www.voiceflow.com/" target="_blank">
-                <img src="/img/logos/partners/voiceflow.png" />
-              </a>
             </div>
-            <div className="partner p-2">
-              <a href="https://cloud.google.com/" target="_blank">
-                <img src="/img/logos/partners/gcloud.png" />
-              </a>
-            </div>
-            <div className="partner p-5">
-              <a href="https://www.wolframalpha.com/" target="_blank">
-                <img src="/img/logos/partners/wolfram.png" />
-              </a>
-            </div>
-            <div className="partner p-5">
-              <a href="https://www.bugsee.com/" target="_blank">
-                <img src="/img/logos/partners/bugsee.png" />
-              </a>
-            </div>
-            <div className="partner p-5">
-              <a href="https://balsamiq.com/" target="_blank">
-                <img src="/img/logos/partners/balsamiq.png" />
-              </a>
-            </div>
-          </div>
         </div>
       </section>
 
       <section className="sponsors">
         <div className="container">
           <h1>Sponsors</h1>
+         
+          <div className="sponsors-grid">
+          <div className="sponsor p-4">
+              <a href="https://www.voiceflow.com/" target="_blank">
+                <img src="/img/logos/partners/voiceflow.png" />
+              </a>
+            </div>
+            <div className="sponsor p-2">
+              <a href="https://cloud.google.com/" target="_blank">
+                <img src="/img/logos/partners/gcloud.png" />
+              </a>
+            </div>
+            <div className="sponsor p-5">
+              <a href="https://www.wolframalpha.com/" target="_blank">
+                <img src="/img/logos/partners/wolfram.png" />
+              </a>
+            </div>
+            <div className="sponsor p-5">
+              <a href="https://www.bugsee.com/" target="_blank">
+                <img src="/img/logos/partners/bugsee.png" />
+              </a>
+            </div>
+            <div className="sponsor p-5">
+              <a href="https://balsamiq.com/" target="_blank">
+                <img src="/img/logos/partners/balsamiq.png" />
+              </a>
+            </div>
+          </div>
           <p>
             Interested in sponsoring us? Check out our{" "}
             <a
