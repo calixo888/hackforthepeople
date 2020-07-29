82d55883667a7cf2b6aea1f89ca986fa431d11d9
Kevin Wang <kevinw@berkeley.edu>
2020-07-29 08:05:22 -0700

    Fix text

--- pages/faqs.js
+++ pages/faqs.js
@@ -36,7 +36,7 @@ export default () => {
           />
           <Accordion
             title="Who can participate?"
-            content="Anyone in highschool and up can participate! There are no restrictions on experience or location."
+            content="Anyone in high school and up can participate! There are no restrictions on experience or location."
           />
           <Accordion
             title="Are there prizes?"
